d2b3aea10bcc15a3c077f8eebf2d3ac653ee3e99
Nayum Teixeira <68863624+Nayumt99@users.noreply.github.com>
2024-05-10 08:17:23 -0300

    Create 06-Ordenacao-de-pocoes-com-bubble-sort.c

---
+++ Desafios/06-Ordenacao-de-pocoes-com-bubble-sort.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void trocar(int *a, int *b){ 
+    int temp = *a; 
+    *a = *b; 
+    *b = temp; 
+}
+
+void bubbleSort(int array[], int quantidade){ 
+  for (int i = 0; i < quantidade - 1; i++) {
+    for (int j = 0; j < quantidade - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        trocar(&array[j], &array[j + 1]);
+      }
+    }
+  }
+}
+
+int main() {
+  // Lê os nossos dados de entrada:
+  int quantidade;
+  scanf("%d", &quantidade);
+  int frascos[quantidade];
+  for (int i = 0; i < quantidade; i++) {
+      scanf("%d", &frascos[i]);
+  }
+  
+  // Executa o algoritmo "bubbleSort" para ordenar os "frascos".
+  bubbleSort(frascos, quantidade);
+  
+  // Imprime os "frascos" ordenados.
+  for (int i = 0; i < quantidade; i++) {
+      printf("%d ", frascos[i]);
+  }
+  
+  return 0;
+}
